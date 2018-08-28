96521dd2b70b84cae3009970e671ad7dbfa0ca6b
Andy Cochran <acochran@planning.nyc.gov>
2018-08-28 12:24:55 -0400

    semantically rename layer-menu-item component (and associated classes) to layer-group-toggle

---
+++ tests/integration/components/labs-ui/layer-group-toggle-test.js
@@ -0,0 +1,41 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render, find, click } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | layer-group-toggle', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it opens and closes on click', async function(assert) {
+    await render(hbs`{{labs-ui/layer-group-toggle title='Foo'}}`);
+    await click('.layer-group-toggle-label');
+    const content = find('.layer-group-toggle-content');
+    assert.equal(!!content, false);
+
+    await click('.layer-group-toggle-label');
+    const content2 = find('.layer-group-toggle-content');
+    assert.equal(!!content2, true);
+  });
+
+  test('it yields content when open', async function(assert) {
+    await render(hbs`
+      {{#labs-ui/layer-group-toggle title='Foo'}}
+        template block text
+      {{/labs-ui/layer-group-toggle}}
+    `);
+    const content = find('.layer-group-toggle-content').textContent.trim();
+    assert.equal(content, 'template block text');
+  });
+
+  test('it shows a title', async function(assert) {
+    await render(hbs`{{labs-ui/layer-group-toggle title='Foo'}}`);
+    const title = await find('.layer-group-toggle-header .layer-group-toggle-label').textContent.trim();
+    assert.equal(title, 'Foo');
+  });
+
+  test('accepts a for property to lookup an object', async function(assert) {
+    await render(hbs`{{labs-ui/layer-group-toggle title='Foo'}}`);
+    const title = await find('.layer-group-toggle-header .layer-group-toggle-label').textContent.trim();
+    assert.equal(title, 'Foo');
+  });
+});
