73730571f03ebc00914a1b9805d52e45684c1cdb
Andy Cochran <acochran@planning.nyc.gov>
2018-08-29 14:47:37 -0400

    get font awesome legend icons working with transitions

--- addon/components/labs-ui/legend-icon.js
+++ addon/components/labs-ui/legend-icon.js
@@ -9,28 +9,10 @@ import layout from '../../templates/components/labs-ui/legend-icon';
 export default class LegendIconComponent extends Component {
   layout = layout;
 
-  @computed('icon')
-  get legendColorStyle() {
-    const color = this.get('icon.options.color');
-    return `color: ${color};`;
-  }
-
-  @computed('icon')
-  get legendBackgroundColorStyle() {
-    const color = this.get('icon.options.color');
-    return `background-color: ${color};`;
-  }
-
-  @computed('icon')
-  get legendBorderColorStyle() {
-    const color = this.get('icon.options.color');
-    return `border-color: ${color};`;
-  }
-
-  @computed('icon')
-  get legendIconType() {
-    const color = this.get('icon.type');
-    return `border-color: ${color};`;
+  @computed
+  get iconType() {
+    const type = this.get('icon.type');
+    return (type === 'fa-icon') ? 'fa-layers' : type;
   }
 
   @argument
