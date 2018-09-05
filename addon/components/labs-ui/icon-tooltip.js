f3664ce854f02226c7e30cd40648039170bb5f42
Andy Cochran <acochran@planning.nyc.gov>
2018-09-05 16:51:39 -0400

    use classNames decorator in icon-tooltip component js

--- addon/components/labs-ui/icon-tooltip.js
+++ addon/components/labs-ui/icon-tooltip.js
@@ -1,13 +1,13 @@
 import Component from '@ember/component';
 import { argument } from '@ember-decorators/argument';
 import { tagName } from '@ember-decorators/component';
+import { classNames } from '@ember-decorators/component';
 import layout from '../../templates/components/labs-ui/icon-tooltip';
 
 @tagName('span')
+@classNames('icon-tooltip')
 export default class InfoTooltip extends Component {
   layout = layout
-
-  classNames=['icon-tooltip'];
 
   @argument
   tip = ''
