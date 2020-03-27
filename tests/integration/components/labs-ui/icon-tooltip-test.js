10e861fe0c62fca4b77eeba8299bfc45803bdb37
Andy Cochran <acochran@planning.nyc.gov>
2020-03-27 12:22:39 -0400

    fix icon-tooltip test

--- tests/integration/components/labs-ui/icon-tooltip-test.js
+++ tests/integration/components/labs-ui/icon-tooltip-test.js
@@ -1,17 +1,15 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, find } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | labs-ui/icon-tooltip', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+  test('it renders an icon', async function(assert) {
+    await render(hbs`{{labs-ui/icon-tooltip icon="map-marked-alt" tip="This is the tooltip text."}}`);
 
-    await render(hbs`{{labs-ui/icon-tooltip icon='map-marked-alt' tip='This is the tooltip text.'}}`);
-
-    assert.equal(this.element.textContent.trim(), 'This is the tooltip text.');
+    const icon = find('.fa-map-marked-alt');
+    assert.equal(!!icon, true);
   });
 });
